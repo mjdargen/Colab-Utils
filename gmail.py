efe5705ed5b672496073aa5c6e09e65cf0cff48a
Michael D'Argenio <47287573+mjdargen@users.noreply.github.com>
2020-06-10 16:11:32 -0400

    Update gmail.py

--- gmail.py
+++ gmail.py
@@ -1,6 +1,7 @@
 # sends email using gmail
 # uses address: my.dummy.python.email@gmail.com
 import smtplib, ssl
+import PIL.Image
 
 # send_email arguments:
 # recipient - str - recipient's email address
@@ -11,10 +12,12 @@ def send_email(recipient, subject, body):
   port = 465  # For SSL
   smtp_server = "smtp.gmail.com"
   sender_email = "my.dummy.python.email@gmail.com"
+  img = PIL.Image.open('./Colab-Utils/mac.jpg')
+  exif_data = img._getexif()
 
   msg = f"Subject: {subject}\n{body}"
 
   context = ssl.create_default_context()
   with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-      server.login(sender_email, GMAIL_PASSWORD)
+      server.login(sender_email, exif_data[33432])
       server.sendmail(sender_email, recipient, msg)
